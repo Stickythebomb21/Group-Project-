bb1d3196c0de5fae1f19c8507c934578708f544d
Stickythebomb21 <79219277+Stickythebomb21@users.noreply.github.com>
2021-02-17 09:45:35 -0800

    Add files via upload

---
+++ Test/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "Hello there";
+
+          //   The form should indicate how many images are in the chosen folder.How you show this is your team choice
+           // The form should also show the current displayed image name, not path. Again, the 'how' is your team choice
+          //The program should be menu-driven, meaning you will mostly use the menu control for basic navigation and choices
+            //Navigating back and forth between images in a folder should be accomplished by using the left or right arrows on the 
+           // keyboard, or by pressing(L)ast or(N)ext keys. Navigation should also show unobtrusive left and right arrows
+
+
+
+
+        }
+    }
+}
